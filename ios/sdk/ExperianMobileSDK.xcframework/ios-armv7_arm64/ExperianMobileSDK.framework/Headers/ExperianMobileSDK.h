0f78cc6848ac9ec99f6d66aed045ab568f14a904
Ezequiel <ezequiel.ifsp@gmail.com>
2020-04-03 17:47:02 -0300

    adding sdk

---
+++ ios/sdk/ExperianMobileSDK.xcframework/ios-armv7_arm64/ExperianMobileSDK.framework/Headers/ExperianMobileSDK.h
@@ -0,0 +1,19 @@
+//
+//  iOSMobileSDK.h
+//  ExperianSDK
+//
+//  Created by Experian on 06/06/18.
+//  Copyright © 2018 dede.exe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExperianSDK.
+FOUNDATION_EXPORT double ExperianSDKVersionNumber;
+
+//! Project version string for ExperianSDK.
+FOUNDATION_EXPORT const unsigned char ExperianSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExperianSDK/PublicHeader.h>
+
+#import "NSData+SHA.h"
